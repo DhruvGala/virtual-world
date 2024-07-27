59de49079ba7fe23bdfe19239e9a64c969d06bec
Dhruv <dhruv.g123@gmail.com>
2024-07-26 20:13:57 -0700

    clear all

--- js/math/graph.js
+++ js/math/graph.js
@@ -36,12 +36,31 @@ class Graph {
         return false;
     }
 
-    removeRandomPoint(point) {
+    removePoint(point) {
+        const segs = this.getSegmentsWithPoint(point);
+        for (const seg of segs) {
+            this.removeSegment(seg);
+        }
         this.points.splice(this.points.indexOf(point), 1);
     }
 
-    removeRandomSegment(seg) {
+    removeSegment(seg) {
         this.segments.splice(this.segments.indexOf(seg), 1);
+    }
+
+    getSegmentsWithPoint(point) {
+        const segs = [];
+        for (const seg of this.segments) {
+            if (seg.includes(point)) {
+                segs.push(seg);
+            }
+        }  
+        return segs;
+    }
+
+    dispose() {
+        this.points.length = 0;
+        this.segments.length = 0;
     }
 
     draw(ctx) {
