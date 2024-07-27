0d73d642755b42d4792392e7280f86defa45939c
Dhruv <dhruv.g123@gmail.com>
2024-07-26 19:54:09 -0700

    remove points and segment randomly

--- js/math/graph.js
+++ js/math/graph.js
@@ -36,6 +36,14 @@ class Graph {
         return false;
     }
 
+    removeRandomPoint(point) {
+        this.points.splice(this.points.indexOf(point), 1);
+    }
+
+    removeRandomSegment(seg) {
+        this.segments.splice(this.segments.indexOf(seg), 1);
+    }
+
     draw(ctx) {
         for (const seg of this.segments) {
             seg.draw(ctx);
