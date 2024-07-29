e0f34f7a6cafd9f65fba5c2f63a35cc7fb32fbb2
Dhruv <dhruv.g123@gmail.com>
2024-07-29 12:15:47 -0700

    remove points

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -13,21 +13,45 @@ class GraphEditor {
 
     #addEventListeners() {
         this.canvas.addEventListener("mousedown", (evt) => {
+            if (evt.button == 2) { //right-click
+                if (this.hovered) {
+                    this.#removePoint(this.hovered)
+                }
+            }
+            if (evt.button == 0) {  //left-click
+                const mouse = new Point(evt.offsetX, evt.offsetY);
+                if (this.hovered) {
+                    this.selected = this.hovered;
+                    return;
+                }
+                this.graph.addPoint(mouse);
+                this.selected = mouse;
+                this.hovered = mouse;
+            }
+        });
+        this.canvas.addEventListener("mousemove", (evt) => {
             const mouse = new Point(evt.offsetX, evt.offsetY);
             this.hovered = getNearestPoint(mouse, this.graph.points, 10);
-            if (this.hovered) {
-                this.selected = this.hovered;
-                return;
-            }
-            this.graph.addPoint(mouse);
-            this.selected = mouse;
+            
         });
+        this.canvas.addEventListener("contextmenu", (evt) => evt.preventDefault());
+    }
+
+    #removePoint(point) {
+        this.graph.removePoint(point);
+        this.hovered = null;
+        if (this.selected == point) {
+            this.selected = null;
+        }
     }
 
     display() {
         this.graph.draw(this.ctx);
         if (this.selected) {
             this.selected.draw(ctx, {outline: true});
+        }
+        if (this.hovered) {
+            this.hovered.draw(ctx, {fill: true});
         }
     }
 }
