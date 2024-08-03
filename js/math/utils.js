51f5656d7264b9008f2e9c1233985c89d7b28a78
Dhruv <dhruv.g123@gmail.com>
2024-08-03 16:15:27 -0700

    sync with course

--- js/math/utils.js
+++ js/math/utils.js
@@ -1,30 +1,28 @@
-function getNearestPoint(loc, points, threshold =  Number.MAX_SAFE_INTEGER) {
+function getNearestPoint(loc, points, threshold = Number.MAX_SAFE_INTEGER) {
     let minDist = Number.MAX_SAFE_INTEGER;
-    let nearestPoint = null;
-
-    for(const point of points) {
-        const dist = distance(point, loc);
-        if (dist < minDist && dist < threshold) {
-            minDist = dist;
-            nearestPoint = point;
-        }
+    let nearest = null;
+    for (const point of points) {
+       const dist = distance(point, loc);
+       if (dist < minDist && dist < threshold) {
+          minDist = dist;
+          nearest = point;
+       }
     }
-
-    return nearestPoint;
-}
-
-function distance(p1, p2) {
+    return nearest;
+ }
+ 
+ function distance(p1, p2) {
     return Math.hypot(p1.x - p2.x, p1.y - p2.y);
-}
-
-function add(p1, p2) {
+ }
+ 
+ function add(p1, p2) {
     return new Point(p1.x + p2.x, p1.y + p2.y);
-}
-
-function subtract(p1, p2) {
+ }
+ 
+ function subtract(p1, p2) {
     return new Point(p1.x - p2.x, p1.y - p2.y);
-}
-
-function scale(p, scaler) {
+ }
+ 
+ function scale(p, scaler) {
     return new Point(p.x * scaler, p.y * scaler);
-}
+ }
